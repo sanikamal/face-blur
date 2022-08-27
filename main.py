dffd76a456b098247fb543692f90d47134f6b698
maom <maom@maoms-MacBook-Pro.local>
2022-08-27 11:52:28 -0700

    update areas

--- main.py
+++ main.py
@@ -5,8 +5,9 @@ def do_face_blur(input_path, output_path):
     """
     1. read image from the input_path
     2. use mediapipe to detect face
-    3. use cv2 to blur two eyes, nose, mouth with four Rectangles. See areas.png. As long as eyes, nose, mouth are blurred, that's OK.
-    The purpose is to de-identify people's photo.
+    3. use cv2 to heavily blur two Rectangle areas, which covers eyes, nose and mouth. See areas.png.
+    As long as eyes, nose, mouth are blurred, that's OK.
+    The purpose is to de-identify people.
     4. write the result to output_path
 
     """
